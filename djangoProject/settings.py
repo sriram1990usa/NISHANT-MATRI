330862bb74cfc5a06fd9d0159dcd467e9a353a16
sriram <sriram1990usa@gmail.com>
2025-01-01 16:09:41 +0530

    change settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 
-SECRET_KEY = 'django-insecure-z)jw-+3z_@9z+96))tl4+(#=ph1p+))_(*c!h45%8^zg%5b4ao'
+SECRET_KEY = 'change me'
+if SECRET_KEY in os.environ: 
+    SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
